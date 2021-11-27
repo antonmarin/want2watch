9b33e6dc1451d54a1f9560d108db12714ac10551
Anton Marin <antonaryo85@gmail.com>
2021-11-27 10:55:06 +0300

    create received request object
    ref #45

--- backend/src/EventsCollector/Endpoints/Http/Glide/UserAddedWantie/Request.php
+++ backend/src/EventsCollector/Endpoints/Http/Glide/UserAddedWantie/Request.php
@@ -4,20 +4,62 @@ declare(strict_types=1);
 
 namespace EventsCollector\Endpoints\Http\Glide\UserAddedWantie;
 
+use DateTimeImmutable;
+use Exception;
 use Infrastructure\Http\SymfonyHttpKernel\RequestDTO;
-use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 final class Request implements RequestDTO
 {
     private string $request;
+    private string $idempotencyKey;
+    private string $hookKind;
+    private DateTimeImmutable $triggeredAt;
+    private string $appID;
+    private string $wantieTitle;
 
     public function __construct(\Symfony\Component\HttpFoundation\Request $request)
     {
-        $this->request = (string) $request;
+        $this->request = (string)$request;
+        $this->idempotencyKey = $request->get('idempotencyKey');
+        $this->hookKind = $request->get('hookKind');
+        try {
+            $this->triggeredAt = new DateTimeImmutable($request->get('timestamp'));
+        } catch (Exception $e) {
+            throw new BadRequestHttpException($e->getMessage(), $e);
+        }
+        $this->appID = $request->get('appID');
+        $params = $request->get('params');
+        $this->wantieTitle = $params['wantieTitle']['value'];
     }
 
     public function getRequest(): string
     {
         return $this->request;
+    }
+
+    public function getIdempotencyKey(): string
+    {
+        return $this->idempotencyKey;
+    }
+
+    public function getHookKind(): string
+    {
+        return $this->hookKind;
+    }
+
+    public function getTriggeredAt(): DateTimeImmutable
+    {
+        return $this->triggeredAt;
+    }
+
+    public function getAppID(): string
+    {
+        return $this->appID;
+    }
+
+    public function getWantieTitle(): string
+    {
+        return $this->wantieTitle;
     }
 }
